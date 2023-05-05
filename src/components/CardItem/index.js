02409451ba16c8389333fcc3e36d2d024b32ecc8
kalavathibandaru <gn.kalavathi4@gmail.com>
2023-05-05 17:53:19 +0000

    technologyCards

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,17 @@
+// Write your code here.
+import './index.css'
+
+const MainComponent = props => {
+  const {Details} = props
+  const {title, description, imgUrl, className} = Details
+
+  return (
+    <li className={className}>
+      <h1 className="sub-heading">{title}</h1>
+      <p className="sub-paragraph">{description}</p>
+      <img src={imgUrl} className="image" alt={title} />
+    </li>
+  )
+}
+
+export default MainComponent
